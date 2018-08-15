23c142f707b1b8cdfdf8da370f5693eb4b605662
Darío Alonso <dariovillar@hotmail.com>
2018-08-15 14:00:38 +0200

    Initial commit

--- OpacityListView/OpacityListView/MainPage.xaml.cs
+++ OpacityListView/OpacityListView/MainPage.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace OpacityListView
@@ -12,6 +8,21 @@ namespace OpacityListView
         public MainPage()
         {
             InitializeComponent();
+
+            ExampleList.ItemsSource = new List<CellModel>
+            {
+                new CellModel {Name = "Torremolinos", Photo = "Malaga.jpg", Opacity = 0.4},
+                new CellModel {Name = "Benalmadena", Photo = "Malaga01.jpg", Opacity = 0.6},
+                new CellModel {Name = "Marbella", Photo = "Malaga02.png", Opacity = 0.8},
+                new CellModel {Name = "Nerja", Photo = "Malaga03.jpg", Opacity = 1}
+            };
         }
+    }
+
+    public class CellModel
+    {
+        public string Name { get; set; }
+        public string Photo { get; set; }
+        public double Opacity { get; set; }
     }
 }
